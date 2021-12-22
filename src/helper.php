1d2cfd0ffb7bfd8fba22d1957d63a30fe44f91b8
xiongy <15810265860@163.com>
2021-12-22 14:36:25 +0800

    helper

--- src/helper.php
+++ src/helper.php
@@ -1,1 +1,29 @@
 <?php
+
+if (!function_exists('ding')) {
+    /**
+     * DingTalk
+     * @param array $config
+     * @return \Xyu\Dingtalk\DingApp
+     * Author: xyu
+     */
+    function ding(array $config)
+    {
+        return (new \Xyu\Dingtalk\DingApp($config));
+    }
+}
+
+if (!function_exists('dingtalk')) {
+    /**
+     * DingTalk
+     * @param string $name
+     * @return mixed|\Xyu\Dingtalk\DingApp
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     * Author: xyu
+     */
+    function dingtalk(string $name = 'default')
+    {
+        return \Hyperf\Utils\ApplicationContext::getContainer()->get(\Xyu\Dingtalk\Hyperf\Factory::class)->make($name);
+    }
+}
